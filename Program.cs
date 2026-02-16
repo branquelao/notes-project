8b381cded37a19db57d87ffc4c72d9f183909f62
branquelao <henriquetozzonarciso@gmail.com>
2026-02-16 10:31:52 -0300

    Doing initial setup, Notes CRUD API with EF Core and SQL Server

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using NotesProjectAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Registering DbContext with SQL Server connection string from appsettings.json
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
